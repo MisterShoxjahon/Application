ba2e180b903dbc3ec39aba1b066aa7279855a347
MisterShoxjahon <sobirzanovsohzahon@gmail.com>
2026-01-16 08:41:48 +0500

    adding small details

--- src/storage/json.storage.ts
+++ src/storage/json.storage.ts
@@ -7,11 +7,20 @@ const filePath = path.join(__dirname, "../../data/progress.json");
 
 export class JSONStorage implements StorageStrategy {
   load(): MemorizationProgress[] {
-    if (!fs.existsSync(filePath)) { return [];
+    if (!fs.existsSync(filePath)) {
+      return [];
     }
-    return JSON.parse(fs.readFileSync(filePath, "utf8"))
+    try {
+      return JSON.parse(fs.readFileSync(filePath, "utf8"));
+    } catch (err) {
+      console.error("Failed to parse JSON file:", err);
+      return [];
+    }
   }
   save(data: MemorizationProgress[]): void {
+    if (!fs.existsSync(path.dirname(filePath))) {
+      fs.mkdirSync(path.dirname(filePath), { recursive: true });
+    }
     fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
   }
 }
